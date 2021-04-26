00d9c1df54cac0aeac1cbc7e0e9a85828cd7d88c
Tsjelle Stevens <tsjelle.stevens@codecare.be>
2021-04-26 20:14:16 +0200

    Override Statistics

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -19,17 +19,39 @@ namespace GradeBook.GradeBooks
 
             if (averageGrade >= 80)
                 return 'A';
-            
+
             if (averageGrade >= 60)
                 return 'B';
-            
+
             if (averageGrade >= 40)
                 return 'C';
-            
+
             if (averageGrade >= 20)
                 return 'D';
-     
+
             return 'F';
+        }
+
+        public override void CalculateStatistics()
+        {
+            if (Students?.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+
+            base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students?.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+
+            base.CalculateStudentStatistics(name);
         }
     }
 }
